602cafe76892fbec697d52acbeec94da32fa3a18
Rick Schneider <otaviohenrique.dev@gmail.com>
2023-09-13 14:24:21 -0300

    codigos primitivos de js

---
+++ JS-ALURA/primitivos.js
@@ -0,0 +1,45 @@
+// //tipo number
+// const meuNumero = 3;
+
+// const primeiroNumero = 1;
+// const segundoNumero = 2;
+
+// const operacaoMatematica = primeiroNumero + segundoNumero;
+// // console.log("resultado da operação mátematica é: " +operacaoMatematica)
+
+// const numeroPontoFlutuante = 3.3;
+// const pontoFlutuanteSemZero = .5;
+
+// const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+// // console.log (novaOperacao);
+
+// const texto1 = "Olá Mundo!";
+// const senha = "sdfkgnçfklbnadb";
+// const stringDeNumeros = "123123123";
+// const texto2 = 'Olá Mundo!';
+// const citacao = "o leo disse 'oi!'";
+// // console.log (texto1, texto2,senha, stringDeNumeros, citacao);
+// const meuNome = "taviao"
+// //concatenação
+// // console.log (citacao + meuNome);
+
+// const cidade = "belo horizonte";
+// const input = "Belo Horizonte";
+
+// const inputMinusculo = input.toLowerCase();
+
+// // console.log(cidade === inputMinusculo); // true
+// const cidade1 = "belo horizonte";
+// const input1 = "Belo Horizonte";
+
+// // console.log(cidade1 === input1); // false
+
+// const senha1 = "minhaSenha123"
+// console.log(senha1.length) // 13 caracteres
+
+
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+console.log(primeiroNumero === segundoNumero);

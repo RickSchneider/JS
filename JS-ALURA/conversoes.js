5038f11590e950a2dc5d6a4b92668fcfc0bf9e54
Rick Schneider <otaviohenrique.dev@gmail.com>
2023-09-13 15:17:26 -0300

    atualição dos codigos dos tipos de coversoes

--- JS-ALURA/conversoes.js
+++ JS-ALURA/conversoes.js
@@ -7,11 +7,11 @@
 //== compara valores
 //=== comparação de valores e tipo
 
-const numero = 456;
+const numero = 456; //pode ser convetido de tipo numero para tipo string usando String(456)
 const numeroString = Number("456"); //converte o texto em numero
 
 //Number()
-//String
+//String()
 //console.log(numero === numeroString);
 console.log(numero + numeroString); // soma os dois valores das variaveis
 //console.log(numero + Number(numeroString));

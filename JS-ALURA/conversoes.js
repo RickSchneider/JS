ad4c820c732875be0ae8e34f00c110b44555a8f9
Rick Schneider <otaviohenrique.dev@gmail.com>
2023-09-13 15:15:46 -0300

    atualição dos codigos dos tipos de coversoes

---
+++ JS-ALURA/conversoes.js
@@ -0,0 +1,19 @@
+//tipo de dado
+//booleanos
+
+//conversao implicita
+
+//= atribuição de valor
+//== compara valores
+//=== comparação de valores e tipo
+
+const numero = 456;
+const numeroString = Number("456"); //converte o texto em numero
+
+//Number()
+//String
+//console.log(numero === numeroString);
+console.log(numero + numeroString); // soma os dois valores das variaveis
+//console.log(numero + Number(numeroString));
+
+//conversao explicita

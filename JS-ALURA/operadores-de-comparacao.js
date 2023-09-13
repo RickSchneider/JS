084cf698f6f4d7fdd3d42aec559168f8e4661d5d
Rick Schneider <otaviohenrique.dev@gmail.com>
2023-09-13 16:08:34 -0300

    adição do conteudo de operadores de comparação

---
+++ JS-ALURA/operadores-de-comparacao.js
@@ -0,0 +1,21 @@
+// == comapração implicita
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+console.log(numero === texto);
+
+//typeof informa o tipo da variavel
+console.log(typeof numero);
+console.log(typeof texto);
+
+// == compara o valor
+// === compara o valor e o tipo de dado
+
+//conversao explicita
+Number();
+String();
+
+//vimos tambem um pouco sobre esta questao de comparação no documento conversoes.js

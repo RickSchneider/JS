434433e507c6829262010d96832caf6cab0d02b2
Rick Schneider <otaviohenrique.dev@gmail.com>
2023-09-13 16:24:37 -0300

    Utilização do template Literal

---
+++ JS-ALURA/template-literal.js
@@ -0,0 +1,15 @@
+const nome = "otavio";
+const idade = 2023 - 2000;
+const cidadeDeNascimento = "floripa";
+
+// const apresentacao =
+//   "meu nome é " +
+//   nome +
+//   ", minha idade é " +
+//   idade +
+//   " e nasci na cidade de " +
+//   cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
